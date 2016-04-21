0675be1096bed7cccf93dd77076fbd700a72595e
Joe Healy <joseph.healy@mymail.champlain.edu>
2016-04-21 00:50:16 -0400

    created and linked cardviewcontroller

--- MTGClient/src/mtgclient/view/CardViewController.java
+++ MTGClient/src/mtgclient/view/CardViewController.java
@@ -2,6 +2,7 @@ package mtgclient.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import mtgclient.model.MagicCard;
 
 public class CardViewController {
 	
@@ -16,5 +17,15 @@ public class CardViewController {
 	@FXML
 	private Label ptLabel;
 	
-	
+	public void displayCard(MagicCard card)
+	{
+		cardNameLabel.setText(card.getName());
+		manaCostLabel.setText(card.getManaCost());
+		rulesTextLabel.setText(card.getRulesText());
+		
+		//TODO - filler info
+		typeLabel.setText("Legendary Creature - Human Warrior");
+		ptLabel.setText("4/4");
+		
+	}
 }
